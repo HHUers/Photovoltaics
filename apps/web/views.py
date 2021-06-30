d698a0fd3d9010fd6ef0af4fc3e1dd5652494b08
Suki <1275506441@qq.com>
2021-06-30 13:08:36 +0800

    add base.html

--- apps/web/views.py
+++ apps/web/views.py
@@ -1,7 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
 from django.views import generic
 
@@ -13,3 +12,7 @@ class IndexView(generic.ListView):
 
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html', locals())
+
+
+def page_not_found(request, exception):
+    return render(request, 'error.html')
