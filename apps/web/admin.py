49dbaae2cc73fc3b01ae4c4152edd1b8729d4d5d
2436917927 <44053847+2436917927@users.noreply.github.com>
2021-07-03 16:59:42 +0800

    change search_filter

--- apps/web/admin.py
+++ apps/web/admin.py
@@ -4,7 +4,7 @@ from apps.web.models import *
 
 class projectOverviewAdmin(admin.ModelAdmin):
     list_display = ['projectNo', 'projectName', 'projectType', 'projectStage', 'projectDesign', 'projectDate']
-    search_fields = ['projectNo', 'projectName']
+    #search_fields = ['projectNo', 'projectName']
     list_filter = ['projectNo', 'projectName']
 
 
@@ -14,7 +14,7 @@ admin.site.register(projectOverview, projectOverviewAdmin)
 class siteProfileAdmin(admin.ModelAdmin):
     list_display = ['projectNo', 'location', 'capacity', 'area', 'altitude', 'longtitude', 'latitude', 'radiationMJ',
                     'radiationkwh', 'dipAngle']
-    search_fields = ['projectNo', 'location', 'longtitude', 'latitude']
+    #search_fields = ['projectNo', 'location', 'longtitude', 'latitude']
     list_filter = ['projectNo', 'location', 'longtitude', 'latitude']
 
 
@@ -25,7 +25,7 @@ class temperatureAdmin(admin.ModelAdmin):
     list_display = ['projectNo', 'avgTemperature', 'maxTemperature', 'minTemperature', 'avgMonthTemperature',
                     'breakingGroundDepth',
                     'avgSpeed', 'maxSpeed', 'rainyDays', 'pollutionLevel']
-    search_fields = ['projectNo']
+    #search_fields = ['projectNo']
     list_filter = ['projectNo']
 
 
@@ -36,7 +36,7 @@ class PVSystemAdmin(admin.ModelAdmin):
     list_display = ['projectNo', 'component', 'installedAngle', 'plan', 'inverter',
                     'capacityRatio', 'inclinedRadiation', 'systemEffience', 'avgElectricity',
                     'avgHours', 'firstYearHours', 'firstConnect', 'fullConnect']
-    search_fields = ['projectNo']
+    #search_fields = ['projectNo']
     list_filter = ['projectNo']
 
 
@@ -46,7 +46,7 @@ admin.site.register(PVSystem, PVSystemAdmin)
 class projectApplyForAdmin(admin.ModelAdmin):
     list_display = ['projectName', 'projectType', 'projectStage', 'projectDesign', 'projectDate','projectStatus', 'pass_audit_str']
 
-    search_fields = ['projectName']
+    #search_fields = ['projectName']
     list_filter = ['projectName']
 
 
